ea1e22087e5e5e30ade3eb33524534004eb9a195
leonel47 <waboleonel@gmail.com>
2023-11-19 23:17:51 +0100

    Adding Grid for responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"header" "main" "footer"`,
+        lg: `"header header" "aside main" "footer footer"`,
+      }}
+      gap={1}
+    >
+      <GridItem area="header" bg="red">
+        Header
+      </GridItem>
+      <GridItem area="header">Header</GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="blue">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+      <GridItem area="footer" bg="#3e3e3e">
+        Footer
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
