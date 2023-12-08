2273cd23a8c441f28fc8ceaa5579f041f9ad22e0
leonel47 <waboleonel@gmail.com>
2023-12-08 18:04:47 +0100

    Implement infinite scroll, but page completely reload (need to be fixed)

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -1,4 +1,6 @@
-import { Button, SimpleGrid, Text } from "@chakra-ui/react";
+import { Button, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { GameQuery } from "../App";
 import useGamesQuery from "../hooks/useGamesQuery";
 import GameCard from "./GameCard";
@@ -20,25 +22,43 @@ const GamesGrid = ({ gameQuery }: Props) => {
     isFetchingNextPage,
   } = useGamesQuery(gameQuery);
 
-  if (gamesLoading)
-    return (
-      <SimpleGrid columns={{ sm: 1, md: 3, xl: 4 }} spacing={8} padding={4}>
-        {skeleton.map((i) => (
-          <GameCardSkeleton key={i} />
-        ))}
-      </SimpleGrid>
-    );
+  if (gamesLoading) return <Spinner />;
+  //   return (
+  //     <SimpleGrid columns={{ sm: 1, md: 3, xl: 4 }} spacing={8} padding={4}>
+  //       {skeleton.map((i) => (
+  //         <GameCardSkeleton key={i} />
+  //       ))}
+  //     </SimpleGrid>
+  //   );
+
+  const fetchedGamesTotal =
+    games?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
 
   return (
     <>
       {gamesError && <Text>{gamesError.message}</Text>}
       {!gamesLoading && (
         <>
-          <SimpleGrid columns={{ sm: 1, md: 3, xl: 4 }} spacing={8} padding={4}>
-            {games?.pages.map((page) =>
-              page.results.map((game) => <GameCard key={game.id} game={game} />)
-            )}
-          </SimpleGrid>
+          <InfiniteScroll
+            hasMore={hasNextPage}
+            dataLength={fetchedGamesTotal}
+            loader={<Spinner />}
+            next={() => fetchNextPage()}
+          >
+            <SimpleGrid
+              columns={{ sm: 1, md: 3, xl: 4 }}
+              spacing={8}
+              padding={4}
+            >
+              {games?.pages.map((page, index) => (
+                <React.Fragment key={index}>
+                  {page.results.map((game) => (
+                    <GameCard key={game.id} game={game} />
+                  ))}
+                </React.Fragment>
+              ))}
+            </SimpleGrid>
+          </InfiniteScroll>
           <Button
             size={"sm"}
             ml={3}
